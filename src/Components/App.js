8463996bcb5ee8a98c2e44eb459492e093ec4ed1
Evan Longwood <evanmlongwood@gmail.com>
2019-04-29 22:37:16 -0700

    Access the json

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios'
 
 class App extends Component {
+
+    async componentDidMount() {
+        axios.get('https://dog.ceo/api/breeds/image/random')
+            .then(function (response) {
+                console.log(response)
+            })
+    }
+
     render() {
         return (
-            <h1>hi</h1>
+            <h1>Hi</h1>
         )
     }
 }
