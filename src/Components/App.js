778b0a23593b833a1ed5390716b9fa53c1a5a6b1
Evan <evanmlongwood@gmail.com>
2019-05-03 16:30:07 -0700

    Refactored options to call the api to display breeds.

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,77 +3,73 @@ import axios from 'axios'
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            imageURL: undefined,
-            allBreeds: [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageURL: undefined,
+      allBreeds: [],
     }
+  }
 
-    componentDidMount()  {
-        axios.get('https://dog.ceo/api/breeds/list')
-            .then(response => {
-                this.setState({allBreeds: response.data.message})
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/list')
+      .then(response => {
+        this.setState({allBreeds: response.data.message})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
 
+  handleChange = (event) => {
+    this.setState({breed: event.target.value});
+  };
 
-    handleChange = (event) => {
-        this.setState({breed: event.target.value});
-    };
+  onRandomDogRequest = () => {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+      .then(response => {
+        this.setState({imageURL: response.data.message})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-    onRandomDogRequest = () => {
-        axios.get('https://dog.ceo/api/breeds/image/random')
-            .then(response => {
-                this.setState({imageURL: response.data.message})
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+  onBreedRequest = () => {
+    axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images/random`)
+      .then(response => {
+        this.setState({imageURL: response.data.message})
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  };
 
-    onBreedRequest = () => {
-        axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images/random`)
-            .then(response => {
-                this.setState({imageURL: response.data.message})
-            })
-            .catch(error => {
-                console.log(error)
-            });
-    };
-
-    render() {
-        const {imageURL} = this.state;
-        console.log(this.state.breed);
-        return (
-            <div>
-                {imageURL && <img src={imageURL} alt='dog'/>}
-                <div>
-                    <div>
-                        <button onClick={this.onRandomDogRequest}>
-                            Random!
-                        </button>
-                    </div>
-                    <div>
-                        <select value={this.state.breed} onChange={this.handleChange}>
-                            <option value="empty">Select a breed!</option>
-                            <option value="Shiba">Shiba</option>
-                            <option value="elkhound/norwegian">Norwegian Elkhound</option>
-                            <option value="Akita"> Akita</option>
-                        </select>
-                        <button onClick={this.onBreedRequest}>
-                            {`Get ${this.state.breed || 'Dog'}!`}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const {imageURL} = this.state;
+    return (
+      <div>
+        {imageURL && <img src={imageURL} alt='dog'/>}
+        <div>
+          <div>
+            <button onClick={this.onRandomDogRequest}>
+              Random!
+            </button>
+          </div>
+          <div>
+            <select value={this.state.breed} onChange={this.handleChange}>
+              <option value="empty">Select a breed!</option>
+              {this.state.allBreeds.map(breed => (<option key={breed} value={breed}>{breed}</option>))}
+            </select>
+            <button onClick={this.onBreedRequest}>
+              {`Get ${this.state.breed || 'Dog'}!`}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App
