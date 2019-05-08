d331d7dd342cab1d6b348a01fd683c15ac2b0cab
Evan Longwood <evanmlongwood@gmail.com>
2019-05-07 19:26:03 -0700

    previous image added, functionality doesn't work.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,95 +1,105 @@
 import React, {Component} from 'react';
 import axios from 'axios'
-import { concat } from 'lodash'
+import {concat} from 'lodash'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      index: null,
-      images: [],
-      allBreeds: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: null,
+            images: [],
+            allBreeds: [],
+        }
     }
-  }
 
-  componentDidMount() {
-    axios.get('https://dog.ceo/api/breeds/list')
-      .then(response => {
-        this.setState({allBreeds: response.data.message})
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+    componentDidMount() {
+        axios.get('https://dog.ceo/api/breeds/list')
+            .then(response => {
+                this.setState({allBreeds: response.data.message})
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
 
-  handleChange = (event) => {
-    this.setState({breed: event.target.value});
-  };
+    handleChange = (event) => {
+        this.setState({breed: event.target.value});
+    };
 
-  onRandomDogRequest = () => {
-    axios.get('https://dog.ceo/api/breeds/image/random')
-      .then(response => {
+    onRandomDogRequest = () => {
+        axios.get('https://dog.ceo/api/breeds/image/random')
+            .then(response => {
+                this.setState({
+                    index: this.state.images.length,
+                    images: concat(this.state.images, response.data.message)
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    onBreedRequest = () => {
+        axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images/random`)
+            .then(response => {
+                this.setState({
+                    index: this.state.images.length,
+                    images: concat(this.state.images, response.data.message)
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    };
+
+    onReset = () => {
         this.setState({
-          index: this.state.images.length,
-          images: concat(this.state.images, response.data.message)
+            index: null,
+            images: []
         })
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+    };
 
-  onBreedRequest = () => {
-    axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images/random`)
-      .then(response => {
+    onPreviousImage = () => {
         this.setState({
-          index: this.state.images.length,
-          images: concat(this.state.images, response.data.message)
-        })
-      })
-      .catch(error => {
-        console.log(error)
-      });
-  };
+            index: this.state[-1],
+        });
+    };
 
-  onReset = () => {
-    this.setState({
-      index: null,
-      images: []
-    })
-  };
-
-  render() {
-    const image = this.state.images[this.state.index];
-    return (
-      <div>
-        {image && <img src={image} alt='dog'/>}
-        <div>
-          <div>
-            <button onClick={this.onRandomDogRequest}>
-              Random!
-            </button>
-          </div>
-          <div>
-            <select value={this.state.breed} onChange={this.handleChange}>
-              <option value="empty">Select a breed!</option>
-              {this.state.allBreeds.map(breed => (<option key={breed} value={breed}>{breed}</option>))}
-            </select>
-            <button onClick={this.onBreedRequest}>
-              {`Get ${this.state.breed || 'Dog'}!`}
-            </button>
-          </div>
-          <div>
-            <button onClick={this.onReset}>
-              Restart!
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const image = this.state.images[this.state.index];
+        console.log(this.state.images);
+        return (
+            <div>
+                {image && <img src={image} alt='dog'/>}
+                <div>
+                    <div>
+                        <button onClick={this.onRandomDogRequest}>
+                            Random!
+                        </button>
+                    </div>
+                    <div>
+                        <select value={this.state.breed} onChange={this.handleChange}>
+                            <option value="empty">Select a breed!</option>
+                            {this.state.allBreeds.map(breed => (<option key={breed} value={breed}>{breed}</option>))}
+                        </select>
+                        <button onClick={this.onBreedRequest}>
+                            {`Get ${this.state.breed || 'Dog'}!`}
+                        </button>
+                    </div>
+                    <div>
+                        <button onClick={this.onReset}>
+                            Restart!
+                        </button>
+                        <button onClick={this.onPreviousImage}>
+                            Previous Image
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App
